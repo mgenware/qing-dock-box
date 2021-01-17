d562505afacb29192291ce02bf43ee488092be08
Mgen <m@mgenware.com>
2021-01-17 18:48:04 +0800

    Fix failing tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -18,20 +18,42 @@ function verifyXY(el: Element, x: number, y: number) {
   expect(rect.y - parentRect.y).to.equal(y);
 }
 
-describe('Props', () => {
-  it('Has a default value of 0', async () => {
-    const el: QingDockBox = await fixture(
-      html`<qing-dock-box style=${pStyle}><div style=${cStyle}>A</div></qing-dock-box>`,
-    );
-    const child = el.firstElementChild!;
-    verifyXY(child, gridSize, gridSize);
-  });
+it('Has a default value of 0', async () => {
+  const el: QingDockBox = await fixture(
+    html`<qing-dock-box style=${pStyle}><div style=${cStyle}>A</div></qing-dock-box>`,
+  );
+  const child = el.firstElementChild!;
+  verifyXY(child, gridSize, gridSize);
+});
 
-  it('t', async () => {
-    const el: QingDockBox = await fixture(
-      html`<qing-dock-box t style=${pStyle}><div style=${cStyle}>A</div></qing-dock-box>`,
-    );
-    const child = el.firstElementChild!;
-    verifyXY(child, gridSize, gridSize);
-  });
+it('t', async () => {
+  const el: QingDockBox = await fixture(
+    html`<qing-dock-box t style=${pStyle}><div style=${cStyle}>A</div></qing-dock-box>`,
+  );
+  const child = el.firstElementChild!;
+  verifyXY(child, gridSize, 0);
+});
+
+it('b', async () => {
+  const el: QingDockBox = await fixture(
+    html`<qing-dock-box b style=${pStyle}><div style=${cStyle}>A</div></qing-dock-box>`,
+  );
+  const child = el.firstElementChild!;
+  verifyXY(child, gridSize, gridSize * 2);
+});
+
+it('l', async () => {
+  const el: QingDockBox = await fixture(
+    html`<qing-dock-box l style=${pStyle}><div style=${cStyle}>A</div></qing-dock-box>`,
+  );
+  const child = el.firstElementChild!;
+  verifyXY(child, 0, gridSize);
+});
+
+it('r', async () => {
+  const el: QingDockBox = await fixture(
+    html`<qing-dock-box r style=${pStyle}><div style=${cStyle}>A</div></qing-dock-box>`,
+  );
+  const child = el.firstElementChild!;
+  verifyXY(child, gridSize * 2, gridSize);
 });
